de49d252ebed85ff70bcdf57c40cce255b86f7cc
tant <tutranvan156@gmail.com>
2022-08-13 11:48:58 +0700

    Initial commit

---
+++ src/main/java/com/elca/DevOps/DevOpsApplication.java
@@ -0,0 +1,14 @@
+package com.elca.DevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(DevOpsApplication.class, args);
+	}
+
+}
